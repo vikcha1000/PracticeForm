7d92c7a63359ee28ee4615725a9d237e3e5e8139
PHOENIXIT\vshigorova <vshigorova@fil-it.ru>
2023-05-21 18:24:14 +0300

    рабочий кейс с комментариями исходный простой вариант

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -20,50 +20,43 @@ public class PracticeFormTest {
     }
 
     @Test
-    void positiveScenario(){
+    void successRegistrationTest(){
+
+        //Переменые регистрируемого студента
+        //идеи доработки: вынести все в одтельную переменную, может список, + добавить все переменные студента
+        //идеи доработок 2: когда вынесу переменные в объкт, вынести его в отдельный файл
+        //идея 3: возможность создавать и проверять одним кейсом 2 объекта или вооьще любое количество
         String firstName = "Ivan";
         String lastName = "Ivanov";
         String userEmail = "email@email.ru";
-
         String gender = "Male";
         String userNumber = "1234567890";
         String subjectsContainer_1 = "sc";
         String subjectsContainer_2 = "y";
-        String hobbie_1 = "Reading";
-        String hobbie_2 = "Music";
+        String hobby_1 = "Reading";
+        String hobby_2 = "Music";
         String currentAddress = "My current Address";
 
-
-
-
+        //заполняем форму
         open("/automation-practice-form");
         $(".main-header").shouldHave(text("Practice Form"));
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail);
-
         $(byText(gender)).click();
         $("#userNumber").setValue(userNumber);
-
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("September");
         $(".react-datepicker__year-select").selectOption("1984");
         $(".react-datepicker__day--017").click();
-
-
-
         $("#subjectsContainer").click();
-
         $("#subjectsInput").setValue(subjectsContainer_1);
         $(byText("Computer Science")).click();
         $("#subjectsInput").setValue(subjectsContainer_2);
         $(byText("Biology")).click();
-
-        $(byText(hobbie_1)).click();
-        $(byText(hobbie_2)).click();
+        $(byText(hobby_1)).click();
+        $(byText(hobby_2)).click();
         $("#uploadPicture").uploadFile(new File("src/test/data/sample.jpg"));
-
-
         $("#currentAddress").setValue(currentAddress);
         $("#state").click();
         $(byText("Haryana")).click();
@@ -71,7 +64,7 @@ public class PracticeFormTest {
         $(byText("Panipat")).click();
         $("#submit").click();
 
-
+        //Блок проверок
         $(".modal-title").shouldHave(text("Thanks for submitting the form"));
         $$("td").shouldHave(itemWithText(firstName + " " + lastName));
         $$("td").shouldHave(itemWithText(userEmail));
@@ -79,9 +72,9 @@ public class PracticeFormTest {
         $$("td").shouldHave(itemWithText(userNumber));
         $$("td").shouldHave(itemWithText("17 September,1984"));
         $$("td").shouldHave(itemWithText("Computer Science, Biology"));
-        $$("td").shouldHave(itemWithText("Reading, Music"));
+        $$("td").shouldHave(itemWithText(hobby_1 + ", " + hobby_2));
         $$("td").shouldHave(itemWithText("sample.jpg"));
-        $$("td").shouldHave(itemWithText("My current Address"));
+        $$("td").shouldHave(itemWithText(currentAddress));
         $$("td").shouldHave(itemWithText("Haryana Panipat"));
 
     }
